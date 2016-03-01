836d241b1f9cd3f610d9a711ae4b3cbbf56f2c3b
吴培峰 <wupeifeng@7fgame.com>
2016-03-01 11:47:26 +0800

    模拟迷雾效果

--- src/Shader/FogOfWar.fs
+++ src/Shader/FogOfWar.fs
@@ -13,19 +13,21 @@ smooth in vec2 vVaryingTexCoords;
  
 void main() {
  
-  vec4 normal;
+  	vec4 normal;
 
     normal = texture(colorMap, vVaryingTexCoords.st);
  
-      float z = texture(depthTexture, vVaryingTexCoords.st).r;
-      float z_dc = 2*z - 1;
-      float x_dc = 2*vVaryingTexCoords.s - 1;
-      float y_dc = 2*vVaryingTexCoords.t - 1;
-      vec4 world_pos = screenToWorldMatrix* vec4(x_dc,y_dc,z_dc,1);
-      vec3 FogOFWarUV = world_pos.xyz / world_pos.w;
-      if (FogOFWarUV.x > 0 && FogOFWarUV.x < 20 && FogOFWarUV.z > 0 && FogOFWarUV.z < 20)
-        vFragColor.rgb = vec3(0); 
-      else
-        vFragColor = normal; 
+    float z = texture(depthTexture, vVaryingTexCoords.st).r;
+    float z_dc = 2*z - 1;
+    float x_dc = 2*vVaryingTexCoords.s - 1;
+    float y_dc = 2*vVaryingTexCoords.t - 1;
+    vec4 world_pos = screenToWorldMatrix* vec4(x_dc,y_dc,z_dc,1);
+    vec3 FogOFWarUV = world_pos.xyz / world_pos.w;
+    if (FogOFWarUV.x > 0 && FogOFWarUV.x < 20 && FogOFWarUV.z > 0 && FogOFWarUV.z < 20){
+    	vec4 FogColor = vec4(0.002750,0.026612,0.082870,0.08);
+    	vFragColor = mix(normal, FogColor, 0.67);
+    }
+    else
+    	vFragColor = normal;   
 
 }

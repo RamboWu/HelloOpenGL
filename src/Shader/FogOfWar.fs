93da4fcd55cd770718542d28ff9af260b2acf62d
吴培峰 <wupeifeng@7fgame.com>
2016-03-01 14:40:57 +0800

    中间增加了一个 非迷雾区，来展示一下 亮的时候

--- src/Shader/FogOfWar.fs
+++ src/Shader/FogOfWar.fs
@@ -21,11 +21,21 @@ void main() {
     float z_dc = 2*z - 1;
     float x_dc = 2*vVaryingTexCoords.s - 1;
     float y_dc = 2*vVaryingTexCoords.t - 1;
-    vec4 world_pos = screenToWorldMatrix* vec4(x_dc,y_dc,z_dc,1);
-    vec3 FogOFWarUV = world_pos.xyz / world_pos.w;
-    if (FogOFWarUV.x > 0 && FogOFWarUV.x < 20 && FogOFWarUV.z > 0 && FogOFWarUV.z < 20){
-    	vec4 FogColor = vec4(0.002750,0.026612,0.082870,0.08);
-    	vFragColor = mix(normal, FogColor, 0.67);
+    vec4 tmp = screenToWorldMatrix* vec4(x_dc,y_dc,z_dc,1);
+    vec3 world_pos = tmp.xyz / tmp.w;
+    if (world_pos.x > 0 && world_pos.x < 20 && world_pos.z > 0 && world_pos.z < 20){
+
+    	float dist = distance(vec2(world_pos.x,world_pos.z),vec2(10,10));
+    	if (dist < 2)
+    	{
+    		vFragColor = normal;  
+    	}
+    	else
+    	{
+    		vec4 FogColor = vec4(0.002750,0.026612,0.082870,0.08);
+    		vFragColor = mix(normal, FogColor, 0.67);
+    	}
+    	
     }
     else
     	vFragColor = normal;   
